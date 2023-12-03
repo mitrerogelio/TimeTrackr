148b0fe73d943a442c683c310f5c8f7bbc8acd8c
rogelio <rogelinmitre@gmail.com>
2023-12-02 21:35:37 -0600

    initial commit

---
+++ TimeTrackr.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace TimeTrackr.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1() { }
+}
---
+++ TimeTrackr/Program.cs
@@ -0,0 +1,19 @@
+﻿using TimeTrackr;
+
+while (true) // Infinite loop
+{
+    Console.WriteLine("\nEnter 'start' to input times, 'exit' to quit, or anything else to continue running.");
+
+    string? command = Console.ReadLine();
+
+    if (command != null && command.Equals("exit", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("See you later!");
+        break;
+    }
+
+    if (command != null && command.Equals("start", StringComparison.OrdinalIgnoreCase))
+    {
+        InputService.ProcessTimeInput();
+    }
+}
